b584cc761647773c7660e0e25e337dc14de3b2b7
melokda <melokda@wpi.edu>
2019-02-09 14:45:54 -0500

    implemented Ben's comments

--- SW/main/Thermal.py
+++ SW/main/Thermal.py
@@ -11,7 +11,7 @@ from scipy.interpolate import griddata
 
 from colour import Color
 
-class Thermal:
+class ThermalMeasure:
 
     def __init__(self):
 
@@ -21,54 +21,10 @@ class Thermal:
 
         #high range of the sensor (this will be red on the screen)
         self.MAXTEMP = 100
-
-        #how many color values we can have
-        self.COLORDEPTH = 1024
-
-        os.putenv('SDL_FBDEV', '/dev/fb1')
-        pygame.init()
-
+        
         #initialize the sensor
         self.sensor = Adafruit_AMG88xx()
-
-        self.points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0, 64)]
-        self.grid_x, self.grid_y = np.mgrid[0:7:32j, 0:7:32j]
-
-        #sensor is an 8x8 grid so lets do a square
-        self.height = 240
-        self.width = 240
-
-        #the list of colors we can choose from
-        self.blue = Color("indigo")
-        self.colors = list(self.blue.range_to(Color("red"), self.COLORDEPTH))
-
-        #create the array of colors
-        self.colors = [(int(c.red * 255), int(c.green * 255), int(c.blue * 255)) for c in self.colors]
-
-        self.displayPixelWidth = self.width / 30
-        self.displayPixelHeight = self.height / 30
-
-        self.lcd = pygame.display.set_mode((self.width, self.height))
-
-        self.lcd.fill((255,0,0))
-
-        pygame.display.update()
-        pygame.mouse.set_visible(False)
-
-        self.lcd.fill((0,0,0))
-        pygame.display.update()
-
-    #some utility functions
-    def constrain(self, val, min_val, max_val):
-        return min(max_val, max(min_val, val))
-
-    @staticmethod
-    def map(x, in_min, in_max, out_min, out_max):
-      return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
-
-    #let the sensor initialize
-    #time.sleep(.1)
-  
+        
     def update(self, stove):
 
         #read the pixels
@@ -110,15 +66,4 @@ class Thermal:
         stove.upper_left.temp = UL_temp
         stove.lower_left.temp = LL_temp
         stove.lower_right.temp = LR_temp
-        
-        pixels = [self.map(p, self.MINTEMP, self.MAXTEMP, 0, self.COLORDEPTH - 1) for p in pixels]
-
-        #perdorm interpolation
-        bicubic = griddata(self.points, pixels, (self.grid_x, self.grid_y), method='cubic')
-        
-        #draw everything
-        for ix, row in enumerate(bicubic):
-            for jx, pixel in enumerate(row):
-                pygame.draw.rect(self.lcd, self.colors[self.constrain(int(pixel), 0, self.COLORDEPTH- 1)], (self.displayPixelHeight * ix, self.displayPixelWidth * jx, self.displayPixelHeight, self.displayPixelWidth))
-                
-        pygame.display.update()
+    
--- SW/main/main.py
+++ SW/main/main.py
@@ -1,26 +1,27 @@
 import firebase_admin
 import time
 from firebase_admin import credentials, db
-from fisheye_usage_3 import capture
+from capture_undistorted import capture
 
 from Stove import Stove
-from Thermal import Thermal
+from Thermal import ThermalMeasure
 
+ITERATION_TIME = 7
 cred = credentials.Certificate('./ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://the-rat-magic.firebaseio.com/'})
 
 db = db.reference("Stove")
 
-thermal = Thermal()
+thermal = ThermalMeasure()
 stove = Stove()
 
 while (True):
-
-    capture()
     
     for burner in [stove.upper_left, stove.lower_left, stove.lower_right, stove.upper_right]:
         db.child("burners").child(burner.name).set(vars(burner))
 
     thermal.update(stove)
 
-    time.sleep(3)
+    capture()
+
+    time.sleep(ITERATION_TIME)
