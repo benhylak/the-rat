06a2f8ce02f44e458f5e1d4386a239778a6be948
melokda <melokda@wpi.edu>
2019-02-13 14:05:31 -0500

    implemented Ben's comments

--- SW/main/main.py
+++ SW/main/main.py
@@ -14,33 +14,38 @@ ITERATION_TIME = 0.5
 parser = argparse.ArgumentParser()
 parser.add_argument('--detect-pots', action='store_true')
 parser.add_argument('--update-time', type=int)
-args = parser.parse_args()
 
 cred = credentials.Certificate('/home/pi/the-rat/SW/main/ServiceAccountKey.json')
 default_app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://the-rat-magic.firebaseio.com/'})
 
 db = db.reference("Stove")
 
-thermal = ThermalMeasure()
-stove = Stove()
-pot_detector = PotDetector()
+def main(detect_pots, update_time):
 
-while (True):
-    
-    for burner in [stove.upper_left, stove.lower_left, stove.lower_right, stove.upper_right]:
-        db.child("burners").child(burner.name).set(vars(burner))
+    thermal = ThermalMeasure()
+    stove = Stove()
+    pot_detector = PotDetector()
 
-    image_path = capture()
+    while (True):
+        
+        for burner in [stove.upper_left, stove.lower_left, stove.lower_right, stove.upper_right]:
+            db.child("burners").child(burner.name).set(vars(burner))
 
-    with open(image_path, mode="rb") as frame:
-        if(args.detect_pots == True):
-            pot_detector.update(frame, stove)
+        image_path = capture()
 
-    thermal.update(stove)
-    
-    if(args.update_time != None):
-        update_time = args.update_time
-    else:
-        update_time = ITERATION_TIME
+        with open(image_path, mode="rb") as frame:
+            if(detect_pots == True):
+                pot_detector.update(frame, stove)
 
-    time.sleep(update_time)
+        thermal.update(stove)
+        
+        if(update_time != None):
+            iteration_time = args.update_time
+        else:
+            iteration_time = ITERATION_TIME
+
+        time.sleep(iteration_time)
+        
+if __name__ == '__main__':
+        args = parser.parse_args()
+        main(args.detect_pots, args.update_time)
